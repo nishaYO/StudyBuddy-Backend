553a8d20c249291d0ef990e222ab4ee10dfc8065
Nisha Singhal <nishasinghal979@gmail.com>
2023-12-07 19:09:51 +0530

    signup resolver graphql added

--- index.js
+++ index.js
@@ -1,25 +1,67 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+const { ApolloServer, gql } = require('apollo-server-express');
 const cors = require('cors');
-const contactRoutes = require('./routes/contactRoutes');
-const sessionRoutes = require('./routes/sessionRoutes');
-const reportsRoutes = require('./routes/reportsRoutes');
-
-require('dotenv').config();// Load environment variables from .env file
 
 const app = express();
-const PORT = process.env.PORT || 5000;
-
-// Middleware
 app.use(cors());
-app.use(bodyParser.json());
 
-// Routes
-app.use('/contact', contactRoutes);
-app.use('/session', sessionRoutes);
-app.use('/reports', reportsRoutes);
+const typeDefs = gql`
+  type Query {
+    placeholder: String
+  }
 
-// Start the server
-app.listen(PORT, () => {
-  console.log(`Server is running on port: ${PORT}`);
+  type User {
+    name: String!
+    email: String!
+  }
+
+  type AuthPayload {
+    user: User
+    token: String
+  }
+
+  type Mutation {
+    signup(name: String!, email: String!, password: String!): AuthPayload
+  }
+`;
+
+const resolvers = {
+  Query: {
+    placeholder: () => "Hello, World!", // Add a placeholder query resolver
+  },
+  Mutation: {
+    signup: (_, { name, email, password }) => {
+      // In a real application, you would handle user signup logic here
+      // For simplicity, return a dummy response
+      const user = {
+        name,
+        email,
+      };
+
+      const token = 'your_jwt_token_here';
+
+      return {
+        user,
+        token,
+      };
+    },
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
 });
+
+// Make sure to await server.start() before applying middleware and starting the server
+async function startServer() {
+  await server.start();
+  server.applyMiddleware({ app });
+
+  const PORT = process.env.PORT || 4000;
+  app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}${server.graphqlPath}`);
+  });
+}
+
+startServer();
